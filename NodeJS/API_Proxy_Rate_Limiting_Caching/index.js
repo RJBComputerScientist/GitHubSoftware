8fb90b5c2e213939dc5b915fb97d65e09414d8ee
Ryan Brooks <RJBComputerScientist@gmail.com>
2023-03-12 21:02:22 -0400

    Backend logic is complete and this project will be enhanced over time

--- NodeJS/API_Proxy_Rate_Limiting_Caching/index.js
+++ NodeJS/API_Proxy_Rate_Limiting_Caching/index.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const cors = require('cors');
+const rateLimit = require('express-rate-limit').rateLimit;
 require('dotenv').config();
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+
+// Rate Limiting .. A type of Proxy
+const Limiter = rateLimit({
+    windowMs: 10 * 60 * 1000, //10 Minutes
+    max: 5
+});
+app.use(Limiter);
+app.set('Trust Proxy', 1)
+
+// Set static folder
+app.use(express.static('public'));
+// How You Will Interact With The API Being Used Depends On The Documentation
 
 // Routes
 app.use('/api', require('./routes'))
