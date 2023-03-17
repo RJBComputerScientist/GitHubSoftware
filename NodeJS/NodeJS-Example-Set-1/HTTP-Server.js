7132ceac2104924221aa06f8c22e13a4ddaec5e9
Ryan Brooks <RJBComputerScientist@gmail.com>
2023-03-17 19:11:38 -0400

    Updated HTTP-Server.js file with notes

--- NodeJS/NodeJS-Example-Set-1/HTTP-Server.js
+++ NodeJS/NodeJS-Example-Set-1/HTTP-Server.js
@@ -10,3 +10,22 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(5000);
+
+// npm - global command, comes with node
+// npm --version
+
+// local dependency - use it only in this particular project
+// npm i <packageName>
+
+// global dependency - use it in any project
+//  npm install -g <packageName>
+// sudo npm install -g <packageName> (mac)
+
+// package.json - manifest file (stores important info about project/package)'
+// manual approach (create package.json in the root, create properties etc)
+// npm init (step by step, press enter to skip)
+// npm init -y (everything default)
+
+// npm install - is to install dependendcies to your project
+// npm uninstall - is to uninstall dependendcies to your project
+// npm update - is to update dependendcies to your project
