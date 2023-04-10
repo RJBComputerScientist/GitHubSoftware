6bb228288be3f8c44cea3e8df1c8551a03b119d8
Ryan Brooks <RJBComputerScientist@gmail.com>
2023-04-10 17:04:24 -0400

    Added 2 extra lines to application.properties for the full configuration of my H2 database

--- Spring_MicroServices/Country_MicroService/src/main/resources/data.sql
+++ Spring_MicroServices/Country_MicroService/src/main/resources/data.sql
@@ -1,3 +1,3 @@
-create table Country (id int primary key, capital varchar(50), country_name varchar(50));
+create table Country (id int primary key, country_name varchar(50), capital varchar(50));
 insert into Country values (1, 'Jamaica', 'Kingston');
 insert into Country values (2, 'USA', 'Washington');
