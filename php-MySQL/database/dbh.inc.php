b3b1fb40cd1f0ca24ffbe032621b7b0a99bc7b2d
Ryan Brooks <RJBComputerScientist@gmail.com>
2024-04-07 12:56:55 -0400

    made a connection file in PHP

---
+++ php-MySQL/database/dbh.inc.php
@@ -0,0 +1,15 @@
+<!-- Here I will create a datasource variable -->
+<!-- MySQLConnection has bad security, MySQLConnectionI has better security than the original -->
+<!-- $pdo stands for php data objects. Another way to connect to databases which is really flexible -->
+<?php
+
+$dsn = "mysql:host=localhost;dbname=test";
+$dbusername = "root";
+$dbpassword = "root";
+
+try {
+    $pdo = new PDO($dsn, dbusername, dbpassword);
+    $pdo -> setAttribute(pdo::ATTR_ERRMODE, pdo::ATTR_ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
