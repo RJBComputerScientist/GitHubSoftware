bc5c4ad9486fb59e35faf6aee460699c2c53f5d7
Ryan Brooks <RJBComputerScientist@gmail.com>
2024-04-09 17:34:28 -0400

    Updated, cleaned and adjusted data and tables for the paintings data

--- php-MySQL/paintings_database/drop-create-paintings.sql
+++ php-MySQL/paintings_database/drop-create-paintings.sql
@@ -14,8 +14,8 @@ DROP TABLE IF EXISTS canvas_size;
 CREATE TABLE if not EXISTS canvas_size (
 	size_id int(4),
     width int(2),
-    height int(2) null,
-    label varchar(15)
+    height int(2),
+    label varchar(30)
     );
 DROP TABLE IF EXISTS museum_hours;
 CREATE TABLE if not EXISTS museum_hours (
@@ -27,25 +27,17 @@ CREATE TABLE if not EXISTS museum_hours (
 DROP TABLE IF EXISTS museum;
 CREATE TABLE if not EXISTS museum (
 	museum_id int(2),
-    name varchar(25),
-    address varchar(50),
-    city varchar(25),
-    state varchar(2),
+    name varchar(50),
+    address varchar(75),
+    city varchar(30),
+    state varchar(25),
     postal varchar(15),
-    country varchar(4),
-    phone varchar(15),
-    url varchar(25)
+    country varchar(50),
+    phone varchar(25),
+    url varchar(150)
     );
 DROP TABLE IF EXISTS subject;
 CREATE TABLE IF NOT EXISTS subject (
     work_id int(6),
     subject varchar(50)
-    );
-DROP TABLE IF EXISTS work;
-CREATE TABLE IF NOT EXISTS work ( 
-	work_id int(6),
-    name varchar(25),
-    artist_id INT(5),
-    style varchar(40),
-    museum_id int(2)
     );
